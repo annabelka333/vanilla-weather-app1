271315880a6aff8842c12a7bdd3671ee8464cf25
annabelka333 <139398794+annabelka333@users.noreply.github.com>
2023-08-06 22:20:05 +0300

    displayed forecast data using Api

--- src/app.js
+++ src/app.js
@@ -21,29 +21,48 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      ` 
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        ` 
         <div class="col">
-            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-date">${formatDay(
+              forecastDay.dt
+            )}</div>
+            
             <img
-                src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
+                src="https://openweathermap.org/img/wn/${
+                  forecastDay.weather[0].icon
+                }@2x.png"
                 alt=""
                 width="42"
             />
         <div class="weather-forecast-temperatures"> 
-            <span class="weather-forecast-temperatures-max"> 18° </span>
-            <span class="weather-forecast-temperatures-min"> 12° </span>
+            <span class="weather-forecast-temperatures-max"> ${Math.round(
+              forecastDay.temp.max
+            )}°</span>
+            <span class="weather-forecast-temperatures-min">${Math.round(
+              forecastDay.temp.min
+            )}°</span>
             </div>
         </div>
         `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
